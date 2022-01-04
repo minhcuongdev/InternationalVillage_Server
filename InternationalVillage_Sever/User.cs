0bace267ba0925a3581340fd7b037419354fcb6f
19520032 <cuongsdbt@gmail.com>
2022-01-04 23:39:59 +0700

    first commit

---
+++ InternationalVillage_Sever/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternationalVillage_Sever
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public string Avatar { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# InternationalVillage_Sever
